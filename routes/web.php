062277209a04c9259d72e93552841924fa994ff7
EC2 Default User <ec2-user@ip-172-31-12-164.ap-northeast-1.compute.internal>
2021-06-04 05:43:01 +0000

    投稿機能追加

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,19 @@ Route::get('/', 'PostsController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/home', 'PostsController@index');
 //ユーザ編集画面
 Route::get('/users/edit', 'UsersController@edit');
 //ユーザ更新画面
 Route::post('/users/update', 'UsersController@update');
 
 Route::get('/users/{user_id}', 'UsersController@show');
+
+Route::get('/posts/new', 'PostsController@new')->name('new');
+
+// 投稿新規処理
+Route::post('/posts','PostsController@store');
+
+Route::get('/postsdelete/{post_id}', 'PostsController@destroy');
